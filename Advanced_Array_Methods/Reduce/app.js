9bffd21957368f85b1213e6a26e37c7b87c429d4
Richard <rickyJ87x@gmail.com>
2022-03-13 20:58:30 -0400

    add Reduce folder

---
+++ Advanced_Array_Methods/Reduce/app.js
@@ -0,0 +1,47 @@
+//extractValue
+
+function extractValue (arr, key){
+    return arr.reduce(function(acc, next){
+        acc.push(next[key]);
+        return acc;
+    },[]);
+}
+
+//vowelCount
+
+function vowelCount (str){
+    const vowels = "aeiou";
+    return str.split('').reduce(function(acc,next){
+        let lowerCased = next.toLowerCase()
+        if(vowels.indexOf(lowerCased) !== -1){
+            if(acc[lowerCased]){
+                acc[lowerCased]++;
+            }else {
+                acc[lowerCased] = 1;
+            }
+        }
+        return acc;
+    }, {});
+}
+
+//addKeyAndValue
+
+function addKeyAndValue (arr){
+    return arr.reduce(function(acc,next,idx){
+        acc[idx][key] = value;
+        return acc;
+    },arr)
+}
+
+//partition
+
+function partition(arr,callback){
+    return arr.reduce(function(acc,next){
+        if(callback(next)){
+            acc[0].push(next);
+        } else {
+            acc[1].push(next);
+        }
+        return acc;
+    }, [[],[]]);
+}
