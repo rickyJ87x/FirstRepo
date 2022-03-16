c4a5e149d7db1166ebd46a5685d8f9e624d463bb
Richard <rickyJ87x@gmail.com>
2022-03-16 17:30:31 -0400

    add ES2015

---
+++ ES2015/let_const_var.js
@@ -0,0 +1,17 @@
+const PI = 3.14;
+
+PI = 42; // Error
+
+
+
+//What is the difference between var and let?
+//var can be redeclared and let cannot
+
+//What is the difference between var and const?
+//var can be redeclared and reassigned but var cannot
+
+//What is the difference between let and const?
+//let can be reassigned and const cannot
+
+//What is hoisting?
+//hoisting is when var is used and the assigned variable is run first in the code regardless of location in code.
